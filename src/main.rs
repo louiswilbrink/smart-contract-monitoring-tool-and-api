239e46ef678158b177b265fd8a95db2960aa0c40
Louis Wilbrink <louis.wilbrink@gmail.com>
2023-01-05 12:41:12 -0500

    * Setup webserver to listen on port 3000. * Set up /transaction endpoint handler.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router
+};
+
+#[tokio::main]
+async fn main() {
+
+    println!("Listening on http://localhost:3000..");
+
+    let app = Router::new().route("/transactions", get(save_transaction));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn save_transaction() {
+    println!("Transaction detected!  Saving..");
 }
