aae11f1d097027b8ba4018d59e6a3d54ea052e10
Louis Wilbrink <louis.wilbrink@gmail.com>
2023-01-05 15:17:46 -0500

    * Added ethers and ethers_provider crates. * Configured JSON-RPC client using Infura provider endpoint. * Added dotenv crate. * Added routines to load configuration variables. * git-ignored `.env` file. * Created `Configuration` struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use dotenv::dotenv;
+
+use std::env::var;
+use std::env::VarError;
+
 use axum::{
     routing::get,
     extract::{Path, Query, Json},
@@ -6,12 +11,23 @@ use axum::{
 
 use std::collections::HashMap;
 
+use ethers::prelude::*;
+
+use ethers_providers::{Provider, Ws, Http, Middleware};
+
 #[tokio::main]
 async fn main() {
+    let config = load_configuration();
+
+    let provider = Provider::<Http>::try_from(config.provider_url).unwrap();
+
+    //provider.subscribe("newPendingTransactions");
+
+    //provider.unsubscribe("newPendingTransactions");
 
     println!("Listening on http://localhost:3000..");
 
-    let app = Router::new().route("/transactions", get(save_transaction));
+    let app = Router::new().route("/transactions", get(process_transaction));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -19,7 +35,29 @@ async fn main() {
         .unwrap();
 }
 
-async fn save_transaction(Query(params): Query<HashMap<String, String>>) {
+async fn process_transaction(Query(params): Query<HashMap<String, String>>) {
     println!("Transaction detected!  Saving..");
     println!("{:?}", params);
+}
+
+pub fn load_configuration() -> Configuration {
+    let config = read_environment_variables();
+
+    match config {
+        Ok(config) => config,
+        Err(_) => panic!("Missing environment variables in the `.env` file.")
+    }
+}
+
+fn read_environment_variables() -> Result<Configuration, VarError> {
+    dotenv().ok();
+
+    Ok(Configuration {
+        provider_url: var("PROVIDER_URL")?
+    })
+}
+
+#[derive(Debug)]
+pub struct Configuration {
+    pub provider_url: String
 }
