645ef9eb37d6daf6ca6e1f850db25e6f542d9e22
Louis Wilbrink <louis.wilbrink@gmail.com>
2023-01-06 08:46:55 -0500

    * Added routine to subscribe to event logs.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# spearbit-take-home
+# Contract Monitoring Tool
 
-This service uses `ether-rs`'s RPC client to listen to transaction events on a contract address.
-
-Make sure to copy and rename the `.env-example` file.
+This service uses `ether-rs`'s RPC client to listen to transaction events on a specified contract address.
 
 The service subscribes to transaction events and stores the following attributes in a Postgres database:
 
@@ -16,3 +14,13 @@ The service also exposes an API endpoint to query journaled transaction data, fi
 
 ### `GET` /transactions
 
+
+
+
+## Getting Started
+
+Copy and rename the `.env-example` file, then add appropriate values.
+
+```
+$ cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use dotenv::dotenv;
 use std::env::var;
 use std::env::VarError;
 
+use std::sync::Arc;
+
 use axum::{
     routing::get,
     extract::{Path, Query, Json},
@@ -17,27 +19,46 @@ use ethers_providers::MAINNET;
 
 use ethers_providers::{Provider, Ws, Http, Middleware};
 
+use ethers_providers::PubsubClient;
+
+use ethers::{
+    core::{
+        abi::AbiDecode,
+        types::{Address, BlockNumber, Filter, U256},
+    },
+};
+
+use u256_literal::u256;
+
+use eyre::Result;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let config = load_configuration();
 
-    println!("Configuration: {:?}", config);
+    let client = Provider::<Ws>::connect("wss://mainnet.infura.io/ws/v3/c60b0bb42f8a4c6481ecd229eddaca27").await?;
 
-    let provider = MAINNET.provider();
+    let client = Arc::new(client);
 
-    match provider.client_version().await {
-        Ok(version) => println!("The version is {}", version),
-        Err(error) => println!("Error getting client version! {:?}", error)
+    let last_block = client.get_block(BlockNumber::Latest).await?.unwrap().number.unwrap();
+
+    println!("last_block: {last_block}");
+
+    let erc20_transfer_filter = Filter::new().from_block(last_block).event("Transfer(address,address,uint256)");
+
+    let mut stream = client.subscribe_logs(&erc20_transfer_filter).await?.take(2);
+
+    while let Some(log) = stream.next().await {
+        println!(
+            "block: {:?}, tx: {:?}, token: {:?}, from: {:?}, to: {:?}, amount: {:?}",
+            log.block_number,
+            log.transaction_hash,
+            log.address,
+            Address::from(log.topics[1]),
+            Address::from(log.topics[2]),
+            U256::decode(log.data),
+        );
     }
-
-    match provider.node_client().await.expect("Error getting node client") {
-        Geth => println!("Provider client is running Geth."),
-        _ => println!("Provider client is not running Geth.")
-    }
-
-    //provider.subscribe("newPendingTransactions");
-
-    //provider.unsubscribe("newPendingTransactions");
 
     println!("Listening on http://localhost:3000..");
 
@@ -47,6 +68,8 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
 async fn process_transaction(Query(params): Query<HashMap<String, String>>) {
