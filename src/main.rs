16e46b16c2a53f293f60f925d18e78026b38533b
Louis Wilbrink <louis.wilbrink@gmail.com>
2023-01-07 20:11:38 -0500

    * Added filtering to database lookups.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use dotenv::dotenv;
-
 use std::{
     env::{var, VarError},
     time::Duration,
@@ -13,8 +11,6 @@ use axum::{
     Router,
 };
 
-use eyre::Result;
-
 use ethers::{
     prelude::*,
     core::{
@@ -26,12 +22,13 @@ use ethers::{
 use ethers_providers::{Provider, Ws};
 
 use sqlx::{
-    Pool,
-    Row,
+    Pool, Row,
     postgres::{Postgres, PgPool, PgPoolOptions, PgRow},
 };
 
-use serde::{Serialize};
+use serde::Serialize;
+use dotenv::dotenv;
+use eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -149,27 +146,68 @@ async fn launch_api(config: &Configuration) -> Result<()> {
     Ok(())
 }
 
-#[derive(Serialize, Debug)]
-struct Transfer {
-    tx_hash: String,
-    sender: String, // TODO: Convert to Ethereum Address type.
-                   // For now, since it's only being sent as JSON to the client,
-                   // we can keep it String for simplicity.
-    recipient: String,
-    amount: f64,
-    timestamp: i64,
-}
-
 async fn get_transactions(State(pool): State<PgPool>, Query(params): Query<HashMap<String, String>>) -> Result<Json<Vec<Transfer>>, (StatusCode, String)> {
 
-    let query = format!(r#"
-        SELECT * 
-        FROM transfers 
-        WHERE {}='{}'
-        "#, 
-        String::from("sender"),
-        String::from("0x33555f2008405660d04f128dc17e6ee01b77c4e7")
-    );
+    // TODO: Find a better way to build filtering.  Using string manipulation for now since sqlx is
+    // not an ORM.
+    let mut query = String::from("SELECT * FROM transfers");
+
+    query.push_str(" WHERE 1=1 "); // WHERE clause added so we can add AND clauses conditionally.
+
+    if params.contains_key("sender") {
+        query.push_str("AND sender='");
+        query.push_str(&params.get("sender").unwrap());
+        query.push_str("'");
+    }
+
+    if params.contains_key("recipient") {
+        query.push_str(" AND recipient='");
+        query.push_str(&params.get("recipient").unwrap());
+        query.push_str("'");
+    }
+
+    if params.contains_key("minAmount") {
+        query.push_str(" AND amount > ");
+        query.push_str(&params.get("minAmount").unwrap());
+    }
+
+    if params.contains_key("maxAmount") {
+        query.push_str(" AND amount < ");
+        query.push_str(&params.get("maxAmount").unwrap());
+    }
+
+    if params.contains_key("before") {
+        query.push_str(" AND timestamp < ");
+        query.push_str(&params.get("before").unwrap());
+    }
+
+    if params.contains_key("after") {
+        query.push_str(" AND timestamp > ");
+        query.push_str(&params.get("after").unwrap());
+    }
+
+    if params.contains_key("order") {
+        if params.get("order").unwrap().eq("asc") {
+            query.push_str(" ORDER BY id");
+        }
+
+        if params.get("order").unwrap().eq("desc") {
+            query.push_str(" ORDER BY id DESC");
+        }
+    }
+
+    if params.contains_key("limit") {
+        query.push_str(" LIMIT ");
+        query.push_str(&params.get("limit").unwrap());
+    }
+
+    if params.contains_key("offset") {
+        query.push_str(" OFFSET ");
+        query.push_str(&params.get("offset").unwrap());
+        query.push_str(" ROWS");
+    }
+
+    println!("Query: {}", query);
 
     // TODO: convert to query_as using custom struct.
     let rows: Vec<PgRow> = sqlx::query(&query)
@@ -189,29 +227,6 @@ async fn get_transactions(State(pool): State<PgPool>, Query(params): Query<HashM
         .collect::<Vec<Transfer>>();
 
     Ok(Json(transfers))
-}
-
-pub fn load_configuration() -> Configuration {
-    let config = read_environment_variables();
-
-    match config {
-        Ok(config) => config,
-        Err(_) => panic!("Missing environment variables in the `.env` file.")
-    }
-}
-
-fn read_environment_variables() -> Result<Configuration, VarError> {
-    dotenv().ok();
-
-    Ok(Configuration {
-        database_name: var("DATABASE_NAME")?,
-        database_username: var("DATABASE_USERNAME")?,
-        database_password: var("DATABASE_PASSWORD")?,
-    })
-}
-
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
 }
 
 async fn get_connection_pool(config: &Configuration) -> Result<Pool<Postgres>, sqlx::Error> {
@@ -243,9 +258,44 @@ async fn get_connection_pool(config: &Configuration) -> Result<Pool<Postgres>, s
     Ok(pool)
 }
 
+pub fn load_configuration() -> Configuration {
+    let config = read_environment_variables();
+
+    match config {
+        Ok(config) => config,
+        Err(_) => panic!("Missing environment variables in the `.env` file.")
+    }
+}
+
+fn read_environment_variables() -> Result<Configuration, VarError> {
+    dotenv().ok();
+
+    Ok(Configuration {
+        database_name: var("DATABASE_NAME")?,
+        database_username: var("DATABASE_USERNAME")?,
+        database_password: var("DATABASE_PASSWORD")?,
+    })
+}
+
+fn _print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 #[derive(Debug)]
 pub struct Configuration {
     pub database_name: String,
     pub database_username: String,
     pub database_password: String,
 }
+
+#[derive(Serialize, Debug)]
+struct Transfer {
+    tx_hash: String,
+    sender: String, // TODO: Convert to Ethereum Address type.
+                   // For now, since it's only being sent as JSON to the client,
+                   // we can keep it String for simplicity.
+    recipient: String,
+    amount: f64,
+    timestamp: i64,
+}
+
